474d401a43a854736df56defc32b2719c779f372
Selina Cleary <selinacleary@Selinas-MacBook-Pro.local>
2015-10-24 21:56:06 -0700

    added jquery to all pages

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-var fadeUntil = 750;
-
+$('body').css('position', 'relative');
 $(window).scroll(function () {
+    var fading = $('.sidebar').find('h2,h3,img, .otherp')
+    fading.each(function () {
+        var offset = $(this).offset()
+        var positionTop = offset.top - $(window).scrollTop();
 
-    var fading = $('.sidebar').find('img, h2, h3')
-    var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-    var offset = scrollBottom,
-        opacity = 0;
-    opacity = offset / fadeUntil;
-    console.log(fading.length)
-    fading.fadeTo(0, opacity)
+        var offsetBottom = $(document).height() - $(this).height() - offset.top;
+
+        var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+        var positionBottom = scrollBottom - offsetBottom
+
+        positionBottom = Math.abs(positionBottom)
+
+        var big = Math.max(positionTop, positionBottom)
+        var small = Math.min(positionTop, positionBottom)
+
+        var opacity = small / big + 0.25
+        $(this).fadeTo(0, opacity)
+
+    });
 });
